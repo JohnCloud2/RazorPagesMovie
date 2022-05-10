6efc53423f07522bec6cd0f7a1918d38128a7da1
TECH4ED-PC5 <TECH4ED-PC5@TECH4ED_5>
2022-05-10 14:00:07 +0800

    Add project files.

---
+++ RazorPagesMovie/Models/SeedData.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RazorPagesMovie.Data;
+
+namespace RazorPagesMovie.Models
+{
+    public static class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new RazorPagesMovieContext(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<RazorPagesMovieContext>>()))
+            {
+                if (context == null || context.Movie == null)
+                {
+                    throw new ArgumentNullException("Null RazorPagesMovieContext");
+                }
+
+                // Look for any movies.
+                if (context.Movie.Any())
+                {
+                    return;   // DB has been seeded
+                }
+
+                context.Movie.AddRange(
+                    new Movie
+                    {
+                        Title = "Who are You",
+                        ReleaseDate = DateTime.Parse("1999-2-15"),
+                        Genre = "Romantic Romance",
+                        Price = 10.99M,
+                        Rating = "R"
+    
+                    },
+
+                    new Movie
+                    {
+                        Title = "Who are you 2 ",
+                        ReleaseDate = DateTime.Parse("2000-3-13"),
+                        Genre = "Romantic Romance",
+                        Price = 10.99M,
+                        Rating = "R"
+
+                    },
+
+                    new Movie
+                    {
+                        Title = "Who are You 3",
+                        ReleaseDate = DateTime.Parse("2001-2-23"),
+                        Genre = "Romantic Romance",
+                        Price =10.99M,
+                        Rating = "R"
+
+                    },
+
+                    new Movie
+                    {
+                        Title = "Who am I",
+                        ReleaseDate = DateTime.Parse("2001-8-14"),
+                        Genre = "Western",
+                        Price = 100.19M,
+                                Rating = "R"
+
+                    }
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
